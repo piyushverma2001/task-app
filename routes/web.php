4f430e5ec011c323944c03103965fce548b885b3
Piyush Verma <piyushverma2001@gmail.com>
2025-06-03 10:44:31 +0530

    Routes Changes

--- routes/web.php
+++ routes/web.php
@@ -25,48 +25,38 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Authentication routes (Laravel's built-in auth routes)
 Auth::routes();
 
-// Profile routes
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-// Admin routes
 Route::middleware(['auth', 'admin'])->prefix('admin')->name('admin.')->group(function () {
-    // Dashboard
     Route::get('/dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
     
-    // User management
     Route::get('/users/register', [AdminUserController::class, 'create'])->name('users.create');
     Route::post('/users', [AdminUserController::class, 'store'])->name('users.store');
     Route::get('/users', [AdminUserController::class, 'index'])->name('users.index');
     Route::post('/register-user', [AdminController::class, 'registerUser'])->name('register.user');
 
-    // Task management
     Route::get('/tasks/create', [AdminTaskController::class, 'create'])->name('tasks.create');
     Route::post('/tasks', [AdminTaskController::class, 'store'])->name('tasks.store');
     Route::get('/tasks', [AdminTaskController::class, 'index'])->name('tasks.index');
     Route::post('/create-task', [AdminController::class, 'createTask'])->name('create.task');
 
-    // Categories
     Route::resource('categories', AdminCategoryController::class);
     Route::get('/get-subcategories', [AdminController::class, 'getSubCategories'])->name('get.subcategories');
 });
 
-// User routes
 Route::middleware(['auth'])->group(function () {
     Route::get('/dashboard', [UserController::class, 'dashboard'])->name('user.dashboard');
     Route::get('/task/{id}', [UserController::class, 'viewTask'])->name('user.task.view');
 });
 
-// Subcategories route (accessible by both admin and users)
 Route::middleware(['auth'])->get('/subcategories/{category}', [SubCategoryController::class, 'getByCategory'])->name('subcategories.fetch');
 
-// Redirect authenticated users based on their role
 Route::get('/home', function () {
     if (auth()->user()->is_admin) {
         return redirect()->route('admin.dashboard');
